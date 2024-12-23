63d8f530365a3cee0e165f42d4c8143de1a68005
Jitin Kumar <jk9120810343@gmail.com>
2024-12-23 14:46:51 +0530

    Add files via upload
    Tuples

---
+++ prob4-13.py
@@ -0,0 +1,9 @@
+Bufflet_menu=('curry','paneer Paties','samosa','Veg Biriyani','chocklates')
+print("Original Menu:")
+for food in Bufflet_menu:
+    print(food.title())
+print("\nModified Menu:")    
+Bufflet_menu=('chhole Bhature','paneer paties','samosa','Burger','choclates')
+for food in Bufflet_menu:
+    print(food.title())
+    
