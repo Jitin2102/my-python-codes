888225381785a481bf2aa692cf168d9605aa6af9
Jitin Kumar <jk9120810343@gmail.com>
2024-12-23 14:21:36 +0530

    Add files via upload
    This is all about Variables in python.

---
+++ boolean.py
@@ -0,0 +1,5 @@
+print(True and False)
+print(not True)
+print(True or False)
+print(not not not  not True)
+print(not(4>5) or (5<6))
---
+++ numbers.py
@@ -0,0 +1,6 @@
+universe_age=14_000_000_000 # Using underscore makes it more readable
+print(universe_age)
+x,y,z=45,65,23     # Multiple Assignment
+print(x," ",y," ",z)
+MAX_CONNECTIONS=5000
+print(MAX_CONNECTIONS)
